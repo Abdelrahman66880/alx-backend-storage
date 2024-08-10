6a579b451ef2bd9ac64897dbf2630664767755ef
Abdelrahman <abdofathi317@gmail.com>
2024-08-10 09:46:48 -0700

    create table if not exist

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,11 +1,7 @@
--- creates a 'users' table if it doesn't already exist.
-
-IF NOT EXISTS (SELECT * FROM sys.tables WHERE type = 'U' AND name = 'users')
-BEGIN
-    CREATE TABLE dbo.users
-    (
-        id INT IDENTITY(1,1) PRIMARY KEY,
-        email VARCHAR(255) NOT NULL UNIQUE,
-        name VARCHAR(255)
-    );
-END
+-- A SQL script that creates a table (called users) with following fields
+-- id, email, name
+CREATE TABLE IF NOT EXISTS users (
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255)
+)
