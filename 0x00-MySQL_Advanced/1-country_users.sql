e22baceb4ad3f0164e4a9630c731b94e52c92f5f
Abdelrahman <abdofathi317@gmail.com>
2024-08-10 10:09:37 -0700

    create table with cretain requirments

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,8 @@
---Create table if not exist with certain requirments
---id, emial, name, country(US, CO, TN)
-
-CREATE TABLE IF NOT EXISTS users (
-	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255),
-	country ENUM('US', 'CO', 'TN') DEFUALT AS 'US' NOT NULL
+-- A SQL script that creates a table (called users) following these requirements:
+-- id, email, name, country(enumeration of US, CO and TN)
+CREATE TABLE IF NOT EXISTS users(
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
 );
